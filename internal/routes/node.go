4e76ac02368f9b0600ee321b86290a44e801f7c0
jvcoutinho <joao.coutinho9@gmail.com>
2023-04-29 15:04:38 -0300

    Add docs for Node

--- internal/routes/node.go
+++ internal/routes/node.go
@@ -2,8 +2,10 @@ package routes
 
 import "strings"
 
+// Node is a representation of either a route path or an HTTP method.
 type Node string
 
+// IsArgument returns true if the current node is an argument (i.e. has the format ":name").
 func (n Node) IsArgument() bool {
 	return strings.HasPrefix(string(n), ":")
 }
