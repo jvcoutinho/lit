a03b745c12ae3e1c0a55f176a0d6350595527f6f
jvcoutinho <joao.coutinho9@gmail.com>
2023-04-29 15:08:51 -0300

    Add Serve method for Router

--- context.go
+++ context.go
@@ -1,4 +1,12 @@
 package lit
 
+import "net/http"
+
 type Context struct {
+	ResponseWriter http.ResponseWriter
+	Request        *http.Request
+}
+
+func NewContext(writer http.ResponseWriter, request *http.Request) *Context {
+	return &Context{writer, request}
 }
--- router.go
+++ router.go
@@ -2,6 +2,7 @@ package lit
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/jvcoutinho/lit/internal/routes"
 )
@@ -33,4 +34,23 @@ func (r *Router) Handle(pattern string, method string, handler func(*Context)) {
 
 	r.graph.Add(route)
 	r.handlers[route.String()] = handler
+}
+
+// ServeHTTP dispatches the request to the handler whose pattern and method most closely matches one previously defined.
+func (r *Router) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	route := routes.NewRoute(request.URL.Path, request.Method)
+
+	ctx := NewContext(writer, request)
+	handler, ok := r.handlers[route.String()]
+	if !ok {
+		http.NotFound(writer, request)
+		return
+	}
+
+	handler(ctx)
+}
+
+// Serve listens on the TCP network address addr and then handle requests on incoming connections.
+func (r *Router) Serve(addr string) error {
+	return http.ListenAndServe(addr, r)
 }
