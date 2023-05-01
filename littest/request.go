6d87dd046fbf6c006f8b62ab8d0d77a877abc5ba
jvcoutinho <joao.coutinho9@gmail.com>
2023-05-01 14:53:04 -0300

    Add littest package

---
+++ littest/request.go
@@ -0,0 +1,20 @@
+// Package littest contains helper functions for testing HTTP routes defined in lit.Router instances.
+package littest
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/jvcoutinho/lit"
+)
+
+// Request requests against router and returns its response.
+func Request(t *testing.T, router *lit.Router, request *http.Request) *http.Response {
+	t.Helper()
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+
+	return recorder.Result()
+}
