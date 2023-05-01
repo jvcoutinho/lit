35c827db89acd1bc22c11d33327a91899135a93e
jvcoutinho <joao.coutinho9@gmail.com>
2023-05-01 15:41:53 -0300

    Make Filter accept any type

--- internal/slices/filter.go
+++ internal/slices/filter.go
@@ -1,7 +1,7 @@
 package slices
 
 // Filter returns all the elements of arr that match predicate.
-func Filter[T comparable](arr []T, predicate func(T) bool) []T {
+func Filter[T any](arr []T, predicate func(T) bool) []T {
 	result := make([]T, 0, len(arr))
 
 	for _, e := range arr {
