a6893f8d37e3346bb904a4ca7ab7adffd2ba30a1
jvcoutinho <joao.coutinho@tino.com.br>
2023-05-07 22:53:47 -0300

    Move Result interface

--- result.go
+++
@@ -1,9 +0,0 @@
-package lit
-
-// Result of an HTTP request.
-//
-// See the lit/render package.
-type Result interface {
-	// Render writes this into the HTTP response managed by ctx.
-	Render(ctx *Context) error
-}
--- router.go
+++ router.go
@@ -9,6 +9,14 @@ import (
 
 const DefaultReadHeaderTimeout = 3 * time.Second
 
+// Result of an HTTP request.
+//
+// See the lit/render package.
+type Result interface {
+	// Render writes this into the HTTP response managed by ctx.
+	Render(ctx *Context) error
+}
+
 // HandleFunc is a function that handles requests.
 type HandleFunc func(ctx *Context) Result
 
