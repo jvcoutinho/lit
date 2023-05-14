7a8fb31043c5ee5c8527be20991dada6ae2d590d
jvcoutinho <joao.coutinho@tino.com.br>
2023-05-14 11:59:16 -0300

    Add tests for Router.ServeHTTP

--- router_test.go
+++ router_test.go
@@ -2,6 +2,7 @@ package lit_test
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/jvcoutinho/lit"
@@ -320,6 +321,247 @@ func TestRouter_Handle(t *testing.T) {
 					router.Handle(test.pattern, test.method, test.handle)
 				})
 			}
+		})
+	}
+}
+
+func TestRouter_ServeHTTP(t *testing.T) {
+	t.Parallel()
+
+	type Route struct {
+		Pattern string
+		Method  string
+		Handle  lit.HandlerFunc
+	}
+
+	type TestCase struct {
+		description          string
+		setUpRouter          func(*lit.Router)
+		existingRoutes       []Route
+		path                 string
+		method               string
+		expectedArguments    map[string]string
+		expectedResponseBody string
+		expectedStatusCode   int
+		expectedHeader       http.Header
+	}
+
+	tests := []TestCase{
+		{
+			description:          "GivenRouterHandlesNoRoutes_ShouldWrite404NotFoundResponse",
+			setUpRouter:          func(r *lit.Router) {},
+			existingRoutes:       []Route{},
+			path:                 "/users",
+			method:               http.MethodGet,
+			expectedArguments:    map[string]string{},
+			expectedResponseBody: "404 page not found\n",
+			expectedStatusCode:   http.StatusNotFound,
+			expectedHeader: http.Header{
+				"Content-Type":           {"text/plain; charset=utf-8"},
+				"X-Content-Type-Options": {"nosniff"},
+			},
+		},
+		{
+			description: "GivenRouteDoesNotExist_MethodDiffers_ShouldWrite404NotFoundResponse",
+			setUpRouter: func(r *lit.Router) {},
+			existingRoutes: []Route{
+				{
+					Pattern: "/users",
+					Method:  http.MethodGet,
+					Handle:  func(ctx *lit.Context) lit.Result { return nil },
+				},
+			},
+			path:                 "/users",
+			method:               http.MethodPost,
+			expectedArguments:    map[string]string{},
+			expectedResponseBody: "404 page not found\n",
+			expectedStatusCode:   http.StatusNotFound,
+			expectedHeader: http.Header{
+				"Content-Type":           {"text/plain; charset=utf-8"},
+				"X-Content-Type-Options": {"nosniff"},
+			},
+		},
+		{
+			description: "GivenRouteDoesNotExist_PatternDiffers_ShouldWrite404NotFoundResponse",
+			setUpRouter: func(r *lit.Router) {},
+			existingRoutes: []Route{
+				{
+					Pattern: "/users",
+					Method:  http.MethodGet,
+					Handle:  func(ctx *lit.Context) lit.Result { return nil },
+				},
+			},
+			path:                 "/books",
+			method:               http.MethodGet,
+			expectedArguments:    map[string]string{},
+			expectedResponseBody: "404 page not found\n",
+			expectedStatusCode:   http.StatusNotFound,
+			expectedHeader: http.Header{
+				"Content-Type":           {"text/plain; charset=utf-8"},
+				"X-Content-Type-Options": {"nosniff"},
+			},
+		},
+		{
+			description: "GivenRouteExists_AndRoutePatternHasStaticSegmentsOnly_ShouldWrite200OKResponse",
+			setUpRouter: func(r *lit.Router) {},
+			existingRoutes: []Route{
+				{
+					Pattern: "/users",
+					Method:  http.MethodGet,
+					Handle:  func(ctx *lit.Context) lit.Result { return nil },
+				},
+			},
+			path:                 "/users",
+			method:               http.MethodGet,
+			expectedArguments:    map[string]string{},
+			expectedResponseBody: "",
+			expectedStatusCode:   http.StatusOK,
+			expectedHeader:       http.Header{},
+		},
+		{
+			description: "GivenRouteExists_AndRoutePatternHasDynamicSegments_ShouldWrite200OKResponse_AndBindArguments",
+			setUpRouter: func(r *lit.Router) {},
+			existingRoutes: []Route{
+				{
+					Pattern: "/users/:id",
+					Method:  http.MethodGet,
+					Handle:  func(ctx *lit.Context) lit.Result { return nil },
+				},
+			},
+			path:                 "/users/Bob",
+			method:               http.MethodGet,
+			expectedArguments:    map[string]string{":id": "Bob"},
+			expectedResponseBody: "",
+			expectedStatusCode:   http.StatusOK,
+			expectedHeader:       http.Header{},
+		},
+		{
+			description: "GivenRouteExists_AndRoutePatternHasMultipleDynamicSegments_ShouldWrite200OKResponse_AndBindArguments",
+			setUpRouter: func(r *lit.Router) {},
+			existingRoutes: []Route{
+				{
+					Pattern: "/users/:user_id/books/:book_id",
+					Method:  http.MethodGet,
+					Handle:  func(ctx *lit.Context) lit.Result { return nil },
+				},
+			},
+			path:                 "/users/Bob/books/123",
+			method:               http.MethodGet,
+			expectedArguments:    map[string]string{":user_id": "Bob", ":book_id": "123"},
+			expectedResponseBody: "",
+			expectedStatusCode:   http.StatusOK,
+			expectedHeader:       http.Header{},
+		},
+		{
+			description: "GivenRouteExists_AndPathHasTrailingSlash_ShouldHandleRouteWithoutTrailingSlash",
+			setUpRouter: func(r *lit.Router) {},
+			existingRoutes: []Route{
+				{
+					Pattern: "/users",
+					Method:  http.MethodGet,
+					Handle: func(ctx *lit.Context) lit.Result {
+						return nil
+					},
+				},
+				{
+					Pattern: "/users/:id",
+					Method:  http.MethodGet,
+					Handle: func(ctx *lit.Context) lit.Result {
+						ctx.SetStatusCode(http.StatusOK)
+
+						return nil
+					},
+				},
+			},
+			path:                 "/users",
+			method:               http.MethodGet,
+			expectedArguments:    map[string]string{},
+			expectedResponseBody: "",
+			expectedStatusCode:   http.StatusOK,
+			expectedHeader:       http.Header{},
+		},
+		{
+			description: "GivenRouteWritesToContext_ShouldReflectInResponse",
+			setUpRouter: func(r *lit.Router) {},
+			existingRoutes: []Route{
+				{
+					Pattern: "/users",
+					Method:  http.MethodGet,
+					Handle: func(ctx *lit.Context) lit.Result {
+						ctx.SetStatusCode(http.StatusBadRequest)
+						ctx.WriteBody([]byte("body"))
+						ctx.SetHeader("Content-Type", "application/json")
+
+						return nil
+					},
+				},
+			},
+			path:                 "/users",
+			method:               http.MethodGet,
+			expectedArguments:    map[string]string{},
+			expectedResponseBody: "body",
+			expectedStatusCode:   http.StatusBadRequest,
+			expectedHeader: http.Header{
+				"Content-Type": {"application/json"},
+			},
+		},
+		{
+			description: "GivenRouteDoesNotExist_AndNotFoundHandlerIsSet_ShouldRunHandler",
+			setUpRouter: func(r *lit.Router) {
+				r.SetNotFoundHandler(func(ctx *lit.Context) lit.Result {
+					ctx.SetStatusCode(http.StatusNotFound)
+					ctx.WriteBody([]byte("not found"))
+
+					return nil
+				})
+			},
+			existingRoutes: []Route{
+				{
+					Pattern: "/users",
+					Method:  http.MethodGet,
+					Handle:  func(ctx *lit.Context) lit.Result { return nil },
+				},
+			},
+			path:                 "/books",
+			method:               http.MethodGet,
+			expectedArguments:    map[string]string{},
+			expectedResponseBody: "not found",
+			expectedStatusCode:   http.StatusNotFound,
+			expectedHeader:       http.Header{},
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.description, func(t *testing.T) {
+			t.Parallel()
+
+			testArgumentsHandler := func(route Route) func(ctx *lit.Context) lit.Result {
+				return func(ctx *lit.Context) lit.Result {
+					require.Equal(t, test.expectedArguments, ctx.URIArguments())
+
+					return route.Handle(ctx)
+				}
+			}
+
+			// Arrange
+			router := lit.NewRouter()
+			test.setUpRouter(router)
+
+			for _, route := range test.existingRoutes {
+				router.Handle(route.Pattern, route.Method, testArgumentsHandler(route))
+			}
+
+			recorder := httptest.NewRecorder()
+			request := httptest.NewRequest(test.method, test.path, nil)
+
+			// Act
+			router.ServeHTTP(recorder, request)
+
+			// Assert
+			require.Equal(t, test.expectedResponseBody, recorder.Body.String())
+			require.Equal(t, test.expectedStatusCode, recorder.Code)
+			require.Equal(t, test.expectedHeader, recorder.Header())
 		})
 	}
 }
